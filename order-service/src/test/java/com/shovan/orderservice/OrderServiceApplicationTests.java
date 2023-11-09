8f931abf1bcead77d8ce953e753485542062c844
shovan <karnashovan2@email.com>
2023-11-09 22:13:27 +0530

    Initialized Order Service files

---
+++ order-service/src/test/java/com/shovan/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shovan.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
